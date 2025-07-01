773dfbf5a0f14deaff60da194162f5aee8144e41
greatgolemn <62272997+greatgolemn@users.noreply.github.com>
2025-07-01 15:54:26 +0700

    Update airtable_chat_app.py

--- airtable_chat_app.py
+++ airtable_chat_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
+st.write("---- DEBUG: st.secrets ----")
 st.write(st.secrets)
+st.write("---------------------------")
 import streamlit as st
 import openai
 import requests
